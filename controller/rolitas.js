404cf4216fabb8af180178463ceae9a2280174f6
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-05 03:51:34 -0600

    routes changed

--- controller/rolitas.js
+++ controller/rolitas.js
@@ -1,5 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
-const Spotify = require("./models/spotify");
+const Spotify = require("../models/spotify");
 
 const token = process.env.TOKEN;
 const parse_mode = "Markdown";
